32d4a8c7cf37993e92b74c4e2564f6c0ad90bd01
David Montaño <cuatro04sr@gmail.com>
2023-02-15 21:52:46 -0500

    add cars toString and print

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         for (int i = 0; i < 10; i++) {
-            //FOR INUTIL
+            // FOR INUTIL
             System.out.println(i);
         }
         System.out.println("Hello world!");
